a0239ac7ba20ecc29e262f8234e36c8b3e98a736
ikshwak31 <br4915@srmist.edu.in>
2026-05-06 23:18:25 +0530

    UC10: Implement draw condition detection

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,60 +1,33 @@
 public class TicTacToe {
 
-    // Global board
     static char[][] board = {
-        {'-', '-', '-'},
-        {'-', '-', '-'},
-        {'-', '-', '-'}
+        {'X', 'O', 'X'},
+        {'X', 'O', 'O'},
+        {'O', 'X', 'X'}
     };
 
     public static void main(String[] args) {
 
-        // Test case
-        board[0][0] = 'X';
-        board[1][1] = 'X';
-        board[2][2] = 'X';
-
-        if (checkWin('X')) {
-            System.out.println("X wins!");
+        if (isDraw()) {
+            System.out.println("Game is a draw!");
         } else {
-            System.out.println("No win yet.");
+            System.out.println("Game is not a draw.");
         }
     }
 
-    static boolean checkWin(char symbol) {
+    // UC10: Check draw condition
+    static boolean isDraw() {
 
-        // Rows
         for (int i = 0; i < 3; i++) {
-            if (board[i][0] == symbol &&
-                board[i][1] == symbol &&
-                board[i][2] == symbol) {
-                return true;
+            for (int j = 0; j < 3; j++) {
+
+                // Empty cell found
+                if (board[i][j] == '-') {
+                    return false;
+                }
             }
         }
 
-        // Columns
-        for (int j = 0; j < 3; j++) {
-            if (board[0][j] == symbol &&
-                board[1][j] == symbol &&
-                board[2][j] == symbol) {
-                return true;
-            }
-        }
-
-        // Diagonal 1
-        if (board[0][0] == symbol &&
-            board[1][1] == symbol &&
-            board[2][2] == symbol) {
-            return true;
-        }
-
-        // Diagonal 2
-        if (board[0][2] == symbol &&
-            board[1][1] == symbol &&
-            board[2][0] == symbol) {
-            return true;
-        }
-
-        return false;
+        return true;
     }
 }
